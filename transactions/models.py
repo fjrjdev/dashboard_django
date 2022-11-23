96ac467e53a3c4612e706cc064e6905ae1025e71
fjrjdev <thedromemafia@gmail.com>
2022-11-23 17:38:33 -0300

    initial commit

---
+++ transactions/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Transaction(models.Model):
+    transaction = models.CharField(max_length=1)
+    date = models.CharField(max_length=10)
+    value = models.FloatField()
+    cpf = models.CharField(max_length=11)
+    card = models.CharField(max_length=12)
+    hour = models.CharField(max_length=8)
+    owner = models.CharField(max_length=14)
+    store = models.CharField(max_length=19)
+
+    created_at = models.DateTimeField(auto_now_add=True)
---
+++ transactions/views.py
@@ -0,0 +1,53 @@
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.views import APIView, Request, Response
+from rest_framework.parsers import MultiPartParser
+import json
+from transactions.models import Transaction
+from transactions.serializers import TransactionSerializer
+
+from utils.clear_data import clear_data
+from utils.write_file import write_file
+
+
+max_length = [1, 8, 10, 11, 12, 6, 14, 19]
+description = [
+    "transaction",
+    "date",
+    "value",
+    "cpf",
+    "card",
+    "hour",
+    "owner",
+    "store",
+]
+
+
+class ListCreateTransictionView(APIView):
+    parser_classes = [MultiPartParser]
+    # queryset = Transaction.objects.all()
+    # serializer_class = TransactionSerializer
+
+    def get(self, request):
+        return Response("sinho")
+
+    def post(self, request, format=None):
+        print(request.data["file"])
+        file_obj = request.data["file"]
+        temp_file = write_file(file_obj=file_obj)
+        if not temp_file:
+            return Response("Error")
+
+        df = clear_data(
+            data_file="media/tmp/CNAB.txt",
+            max_length=max_length,
+            description=description,
+        )
+        json_list = json.loads(json.dumps(list(df.T.to_dict().values())))
+        data = []
+        for dict_item in json_list:
+            instance = Transaction.objects.create(**dict_item)
+            data.append(instance)
+        serializer = TransactionSerializer(data=data, many=True)
+        serializer.is_valid()
+        # print(x)
+        return Response(serializer.data)
