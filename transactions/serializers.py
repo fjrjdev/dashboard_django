c3cfbb2c26dfa57698cb60adf57df1b98bdffc86
fjrjdev <thedromemafia@gmail.com>
2022-11-24 08:51:05 -0300

    feat: transaction serializer update

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -5,4 +5,14 @@ from transactions.models import Transaction
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
-        fields = "__all__"
+        fields = [
+            "transaction",
+            "date",
+            "value",
+            "cpf",
+            "card",
+            "hour",
+            "owner",
+            "store",
+        ]
+        read_only_fields = ["id", "created_at", "updated_at"]
