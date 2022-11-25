276896cb651f93160407671da72e2756cdf623b5
fjrjdev <thedromemafia@gmail.com>
2022-11-25 09:13:05 -0300

    update: variable name

--- utils/write_file.py
+++ utils/write_file.py
@@ -1,11 +1,11 @@
 from django.core.files.storage import default_storage
 
-filename = "CNAB.txt"
+FILENAME = "CNAB.txt"
 
 
 def write_file(file_obj):
     try:
-        with default_storage.open("tmp/" + filename, "wb+") as destination:
+        with default_storage.open("tmp/" + FILENAME, "wb+") as destination:
             for chunk in file_obj.chunks():
                 destination.write(chunk)
         return True
